cc77259189dde7943627867faffb3f9378915648
xiaotong1996 <chen.xiaotong@hotmail.com>
2019-01-09 22:54:18 +0100

    add new function: click show details

--- app/src/main/java/com/example/chenx/musc/tool/RecordAdapter.java
+++ app/src/main/java/com/example/chenx/musc/tool/RecordAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.chenx.musc.R;
@@ -17,19 +18,35 @@ public class RecordAdapter extends RecyclerView.Adapter<RecordAdapter.ViewHolder
 
     private List<Record> mRecordList;
 
+    private int opened= -1;
+
     static class ViewHolder extends  RecyclerView.ViewHolder{
+        View recordView;
         TextView date;
         TextView group;
         TextView action;
+        TextView weight;
+        TextView times;
+        LinearLayout line0;
+        LinearLayout line1;
+        LinearLayout line2;
+
 
         public ViewHolder(View view)
         {
             super(view);
+            recordView=view;
             date=(TextView) view.findViewById(R.id.record_date);
             group=(TextView) view.findViewById(R.id.record_group);
             action=(TextView) view.findViewById(R.id.record_action);
+            weight=(TextView) view.findViewById(R.id.record_weight);
+            times=(TextView) view.findViewById(R.id.record_times);
+            line0 = (LinearLayout) view.findViewById(R.id.record_line0);
+            line1 = (LinearLayout) view.findViewById(R.id.record_line1);
+            line2 = (LinearLayout) view.findViewById(R.id.record_line2);
         }
     }
+
 
     public RecordAdapter(List<Record> recordList){
         mRecordList=recordList;
@@ -37,9 +54,26 @@ public class RecordAdapter extends RecyclerView.Adapter<RecordAdapter.ViewHolder
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, final int i) {
         View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.record_item, parent,false);
-        ViewHolder holder=new ViewHolder(view);
+        final ViewHolder holder=new ViewHolder(view);
+        holder.recordView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int position=holder.getAdapterPosition();
+                if(opened==position)
+                {
+                    opened=-1;
+                    notifyItemChanged(position);
+                }
+                else {
+                    int oldOpened=opened;
+                    opened=position;
+                    notifyItemChanged(oldOpened);
+                    notifyItemChanged(opened);
+                }
+            }
+        });
         return holder;
     }
 
@@ -50,7 +84,18 @@ public class RecordAdapter extends RecyclerView.Adapter<RecordAdapter.ViewHolder
         viewHolder.date.setText(simpleDateFormat.format(record.getDate()));
         viewHolder.group.setText(record.getAction().getGroup());
         viewHolder.action.setText(record.getAction().getName());
+        viewHolder.weight.setText(String.valueOf(record.getWoWeight()));
+        viewHolder.times.setText(String.valueOf(record.getTimes()));
+
+        if(i == opened){
+            viewHolder.line1.setVisibility(View.VISIBLE);
+            viewHolder.line2.setVisibility(View.VISIBLE);
+        }else{
+            viewHolder.line1.setVisibility(View.GONE);
+            viewHolder.line2.setVisibility(View.GONE);
+        }
     }
+
 
     @Override
     public int getItemCount() {
